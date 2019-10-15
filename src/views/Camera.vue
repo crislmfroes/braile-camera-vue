a50dd03a59442b375b0e9e8ab0d0d3e8dcdd9a80
crislmfroes <cris.lima.froes@gmail.com>
2019-10-15 18:33:09 -0300

    commit inicial

---
+++ src/views/Camera.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="home">
+    <h1>Camera</h1>
+    <vue-web-cam v-bind:selectFirstDevice="true"/>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import { WebCam } from "vue-web-cam"
+
+export default {
+  name: 'camera',
+  components: {
+    "vue-web-cam": WebCam
+  }
+}
+</script>
